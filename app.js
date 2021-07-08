c5058be8d58b10ea13860f9efcc4936c0fe3505b
Kerri McMahon <kerrimcm25@gmail.com>
2021-07-08 16:10:03 +0100

    Data added onto new line in file

--- app.js
+++ app.js
@@ -2,28 +2,26 @@ const http = require('http');
 var fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    fs.readFile('index.html', (err, data) => {
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.write(data)
-        res.end()
-    })
-    if(req.method === "POST") {
-      let body = ""
+  fs.readFile('index.html', (err, data) => {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(data)
+    res.end()
+  })
+  if(req.method === "POST") {
+    let body = ""
     req.on('data', chunk => {
       body += chunk.toString();
+  })
+  req.on('end', () => {
+    let userData = body + '\n'
+    fs.appendFile('userData.txt', userData, (err) => {
+      if(err){
+        throw err
+      }
     })
-    req.on('end', () => {
-      console.log(body);
-      fs.writeFile('somefile.txt', body, (err) => {
-        if(err){
-            throw err
-        }
-      })
-      res.end()
-    })}
+  })}
 })
 
 server.listen(3000)
 
 console.log('http://127.0.0.1:3000')
-
