3658ad2a75b4ba75006d4eb8b22b36269223d8dd
Kerri McMahon <kerrimcm25@gmail.com>
2021-07-08 14:58:43 +0100

    Data from form now writing to file

--- app.js
+++ app.js
@@ -1,43 +1,29 @@
 const http = require('http');
 var fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const server = http.createServer((req, res) => {
+    fs.readFile('index.html', (err, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write(data)
+        res.end()
+    })
+    if(req.method === "POST") {
+      let body = ""
+    req.on('data', chunk => {
+      body += chunk.toString();
+    })
+    req.on('end', () => {
+      console.log(body);
+      fs.writeFile('somefile.txt', body, (err) => {
+        if(err){
+            throw err
+        }
+      })
+      res.end()
+    })}
+})
 
-fs.readFile('./index.html', function (err, html) {
+server.listen(3000)
 
-  if (err) throw err;    
-
-  http.createServer(function(request, response) {  
-    response.writeHeader(200, {"Content-Type": "text/html"});  
-    response.write(html);  
-    response.end();  
-  }).listen(port, hostname, () => {
-      console.log(`Server running at http://${hostname}:${port}/`);
-  });
-});
-
-let data = "Learning how to write in a file."
-  
-fs.writeFile('Output.txt', data, (err) => { 
-  if (err) throw err; 
-  console.log(data);
-}) 
-
-function WriteToFile(passForm) {
-  var fso = CreateObject("Scripting.FileSystemObject"); 
-  var s = fso.CreateTextFile("<your Path>/filename.txt", True);
-   
-  var name = document.getElementById('name');
-  var email  = document.getElementById('email');
-  var message  = document.getElementById('message');
-  var checkbox = document.getElementById('checkbox');
-   
-  s.writeline("Name :" + name);
-  s.writeline("Email :" + email);
-  s.writeline("Message :" + message);
-  s.writeline("Subscribed :" + checkbox);
-  s.writeline("-----------------------------");
-  s.Close();
-}
+console.log('http://127.0.0.1:3000')
 
